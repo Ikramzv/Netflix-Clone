739e50488277578469598b51572d444957126553
Ikram <ikramzulfugar@gmail.com>
2022-06-27 00:13:34 +0400

    ADDED: Banner trailer video when clicking play btn

--- src/Banner.js
+++ src/Banner.js
@@ -2,10 +2,13 @@ import React, { useEffect, useMemo, useState } from 'react'
 import axios from './axios'
 import requests from './requests'
 import './Banner.css'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 const base_url = 'https://image.tmdb.org/t/p/original'
 
 function Banner() {
+    const [trailerUrl , setTrailerUrl] = useState('')
     const [movie , setMovie] = useState([])
 
     const memoize = useMemo(() => {
@@ -24,19 +27,46 @@ function Banner() {
 
     console.log(movie);
 
+    const handleClick = (movie) => {
+        movieTrailer(movie?.name || movie?.title || movie?.original_name || '').then((url) => {
+            const urlParams = new URLSearchParams(new URL(url).search)
+            setTrailerUrl(urlParams.get('v'))
+        }).catch((err) => {
+            if(err) return alert(`${movie?.name || movie?.title || movie?.original_name || ''}'s trailer wasn't found`)
+        })
+    }
+
+    const opts = {
+        height: '400px' ,
+        width: '100%' , 
+        host: 'https://www.youtube.com' ,
+        playerVars : {
+            autoplay: 1,
+            controls: 1,
+            loop: 1
+        }
+    }
+
     return (
-        <header className='banner_header' style={{backgroundImage: `url(${base_url}${movie.backdrop_path})`}} >
-            <div className='banner_contents'>
-                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <header className='banner_header' style={{backgroundImage: `${!trailerUrl ? `url(${base_url}${movie.backdrop_path})` : ''}`}} >
+            {!trailerUrl ?  <>
+                            <div className='banner_contents'>
+                                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
 
-                <div className='banner_buttons' >
-                    <button className='banner_button'>Play</button>
-                    <button className='banner_button'>My List</button>
-                </div>
+                                <div className='banner_buttons' >
+                                    <button className='banner_button' onClick={() => handleClick(movie)}>Play</button>
+                                    <button className='banner_button'>My List</button>
+                                </div>
 
-                <h4>{movie?.overview}</h4>
-            </div>
-            <div className='banner_fadeBottom'></div>
+                                <h4>{movie?.overview}</h4>
+                            </div>
+                            <div className='banner_fadeBottom'></div>
+                            </> 
+                            : 
+                            <div className='banner_trailer_video'>
+                                <YouTube videoId={trailerUrl} opts={opts} />
+                                <div className='close_btn' onClick={() => setTrailerUrl('')} >CLOSE</div>
+                            </div> }
         </header>
     )
 }
